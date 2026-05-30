d2433d8d33f49f2da62a0b97df2205f905bcd91f
mariam-zahran21 <memezahran1990@gmail.com>
2026-05-30 16:29:49 +0300

    Fix image loading issue and align testing files and scripts to root

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+import gulp from "gulp";
+import shell from "gulp-shell";
+
+gulp.task(
+  "parcel",
+  shell.task(["npx parcel index.html"])
+);
+
+gulp.task(
+  "test",
+  shell.task(["npx mocha test/shuffle.js"])
+);
+
+gulp.task(
+  "cypress",
+  shell.task(["npx cypress run"])
+);
+
+gulp.task("default", gulp.series("parcel"));
